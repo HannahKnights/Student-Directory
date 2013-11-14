a2272c35c33e63552912d864c0aa638ee8955abd
Hannah Knights <hannahknights@hotmail.co.uk>
2013-11-14 12:14:45 +0000

    Updated student data into an array. Called names using array index

--- directory.rb
+++ directory.rb
@@ -2,20 +2,24 @@ student_count = 14
 
 puts "The students on my cohort at Makers Academy"
 puts "---------------"
-# The above line is Github flavoured Markdown (I think)
-puts "Asta"
-puts "G"
-puts "Hannah"
-puts "Simon"
-puts "Chris"
-puts "Erica"
-puts "Kennedy"
-puts "Annette"
-puts "Lara"
-puts "James"
-puts "Jeremy"
-puts "Giacomo"
-puts "Nisar"
-puts "Bruce"
+
+student=[
+"David Suchet",
+"Meg Ryan",
+"Agatha Christie",
+"Walt Disney",
+"Usain Bolt",
+"Jeremy Kyle",
+"David Hockney",
+"Pat Butcher",
+"Jim Carey",
+"Colin Powell",
+"Kofi Annan",
+"Carole King",
+"Dolly Parton",
+]
+
+puts student[5]
+puts student[0]
 #I should be able to write a ruby code to 'count' the number of student enteries and the print the result in the sentence
 puts "There are #{student_count} students." 
